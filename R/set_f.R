0fe48c060509549f9088d97c53b6a320244e57d3
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-11-24 16:40:10 +0100

    Documented set_f

--- R/set_f.R
+++ R/set_f.R
@@ -1,3 +1,14 @@
+#' Set a function for the numerical optimization.
+#'
+#' @param f An object of class \code{function}.
+#' @param npar The number of parameters in \code{f}.
+#'
+#' @return An object of class \code{ino_f}.
+#' @export
+#'
+#' @examples
+#' f <- function(x) x
+#' set_f(f, 1)
 set_f <- function(f, npar) {
   out <- list(f = f, npar = npar)
   class(out) <- "ino_f"
