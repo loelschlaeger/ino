3116548bc30c2875333b102ae9ce1e6751dc182a
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-11-30 20:49:28 +0100

    set_f now returns an object of class "ino" and can set additional arguments of f. Defined a better example.

--- R/set_f.R
+++ R/set_f.R
@@ -2,21 +2,22 @@
 #'
 #' @param f An object of class \code{function}.
 #' @param npar The number of parameters in \code{f}.
+#' @param ... Additional arguments to be passed to \code{f}.
 #'
 #' @return An object of class \code{ino_f}.
 #' @export
 #'
 #' @examples
-#' f <- function(x) x
-#' set_f(f, 1)
-set_f <- function(f, npar) {
+#' himmelblau <- function(x, c, d) (x[1]^2 + x[2] + c)^2 + (x[1] + x[2]^2 + d)^2
+#' set_f(f = himmelblau, npar = 2, c = -11, d = -7)
+set_f <- function(f, npar, ...) {
   if (class(f) != "function") {
     stop("'f' must be of class function.")
   }
   if (!is.numeric(npar) || npar %% 1 != 0 || npar < 1) {
     stop("'npar' must be a number.")
   }
-  out <- list(f = f, npar = npar)
-  class(out) <- "ino_f"
+  out <- list(f = f, npar = npar, ...)
+  class(out) <- "ino"
   return(out)
 }
--- tests/testthat/test-set_f.R
+++ tests/testthat/test-set_f.R
@@ -1,5 +1,5 @@
 test_that("set_f filters wrong arguments", {
   expect_error(set_f("f", 0))
   expect_error(set_f(function(x) x, 0))
-  expect_s3_class(set_f(function(x) x, 1), "ino_f")
+  expect_s3_class(set_f(function(x) x, 1), "ino")
 })
