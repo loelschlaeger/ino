ca3645797a6802108645270e5b7f82240b06d299
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2022-03-02 14:12:40 +0100

    Fixed bug.

--- R/fixed_initialization.R
+++ R/fixed_initialization.R
@@ -1,9 +1,10 @@
 #' Fixed initialization
 #'
+#' @description
 #' This function implemented the fixed initialization strategy.
 #'
 #' @details
-#' See the vignette ... for more details.
+#' See the vignette "Initialization strategies" for more details.
 #'
 #' @param x
 #' An object of class \code{ino}.
@@ -25,28 +26,23 @@ fixed_initialization <- function(x, at) {
   if (class(x) != "ino") {
     stop("'x' must be of class 'ino'.")
   }
-  if(!(is.list(at))){
+  if(!is.list(at)){
     stop("'at' must be a list.")
   }
-  if(length(at) > x$f$npar){
-    stop("'at' has more entries than the function has parameters.")
+  if(any(sapply(at,length) > x$f$npar)){
+    stop("Some element in 'at' has more entries than the function has parameters.")
   }
-  if(length(at) < x$f$npar){
-    stop("'at' has less entries than the function has parameters.")
+  if(any(sapply(at,length) < x$f$npar)){
+    stop("Some element in 'at' has less entries than the function has parameters.")
   }
 
   ### loop over all parameter combinations provided
-  for(run in 1:length(at[[1]])){
-
-    ### fixed initialization
-    p <- unname(unlist(lapply(at, `[[`, 1)))
+  for(p in at){
     main_args <- list(f = x$f$f, p = p)
     out <- do.call_timed(
       what = x$optimizer$fun,
       args = c(main_args, x$f$add)
     )
-
-    ### save results in ino
     x <- save_optimization_results(x, strategy = "fixed", res = out$res,
                                    time = out$time)
   }
