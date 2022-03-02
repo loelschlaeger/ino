d5d757b29e3530a4974f71d137bdb50d45aa4ff6
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2022-03-02 10:57:37 +0100

    Updated summary function.

--- R/new_ino.R
+++ R/new_ino.R
@@ -1,5 +1,6 @@
 #' Constructor of \code{ino} objects.
 #'
+#' @description
 #' This function constructs an object of class \code{ino}.
 #'
 #' @return
@@ -32,15 +33,37 @@ print.ino <- function(x, ...) {
 #' @noRd
 
 summary.ino <- function(object, ...) {
-  return(list("f_set" = !identical(object$f,NA),
-              "no_data" = length(object$data),
-              "no_optimizer" = length(object$optimizer),
-              "no_optimizations" = length(object$optimizations)))
+  out <- list("f_set" = !identical(object[["f"]],NA),
+              "data_set" = !identical(object[["data"]],list()),
+              "no_data" = length(object[["data"]]),
+              "optimizer_set" = !identical(object[["optimizer"]],list()),
+              "no_optimizer" = length(object[["optimizer"]]),
+              "no_optimizations" = length(object[["optimizations"]]))
+  class(out) <- "summary.ino"
+  return(out)
 }
 
 #' @export
 #' @noRd
+#' @importFrom crayon underline
 
 print.summary.ino <- function(x, ...) {
-  print(x)
+
+  yes_no <- function(bool) {
+    ifelse(bool, crayon::green("\U2713"), crayon::red("\U274C"))
+  }
+
+  ### summarize specifications
+  cat(crayon::underline("Specifications\n"))
+  cat("* f:", yes_no(x$f_set), "\n")
+  cat("* data:", yes_no(x$data_set),
+      ifelse(x$data_set,paste0("(",x$no_data,")"),""),"\n")
+  cat("* optimizer:", yes_no(x$optimizer_set),
+      ifelse(x$optimizer_set,paste0("(",x$no_optimizer,")"),""),"\n")
+
+  ### summarize optimization
+  cat(crayon::underline("\nOptimizations\n"))
+
+  ### summarize evaluations
+  cat(crayon::underline("\nEvaluations\n"))
 }
