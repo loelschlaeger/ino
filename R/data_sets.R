23d932d9cad9da7f81eb8a8a30048431163dd5f4
loelschlaeger <lennart.oelschlaeger@uni-bielefeld.de>
2022-03-25 19:32:24 +0100

    Added data sets.

---
+++ R/data_sets.R
@@ -0,0 +1,66 @@
+#' Earthquake data
+#'
+#' @description
+#' This data set includes the number of yearly measured earthquakes from 1900
+#' to 2006.
+#'
+#' @docType data
+#'
+#' @usage data(earthquakes)
+#'
+#' @format
+#' The data set is a data frame with two integer columns, \code{year} for the
+#' year and \code{obs} for the number of measured earthquakes.
+#'
+#' @source
+#' The data was obtained from
+#' \url{http://hmms-for-time-series.de/second/data/earthquakes.txt}
+#' on 2022-03-25.
+#'
+#' @keywords
+#' dataset
+#'
+"earthquakes"
+
+#' Simulated choice data from a mixed probit model
+#'
+#' @description
+#' This data set includes simulated choices and corresponding choice
+#' characteristics from a (normally) mixed probit model.
+#'
+#' @details
+#' \preformatted{
+#' }
+#'
+#' @docType data
+#'
+#' @usage data(probit)
+#'
+#' @format
+#'
+#' @keywords
+#' dataset
+#'
+"probit"
+
+#' Simulated choice data from a mixed logit model
+#'
+#' @description
+#' This data set includes simulated choices and corresponding choice
+#' characteristics from a (normally) mixed logit model.
+#'
+#' @details
+#' \preformatted{
+#' }
+#'
+#' @docType data
+#'
+#' @usage data(logit)
+#'
+#' @format
+#'
+#' @keywords
+#' dataset
+#'
+"logit"
+
