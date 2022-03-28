bd90f6b581be8802430f2f372ea004e9710cbaa0
marius-oetting <marius.oetting@uni-bielefeld.de>
2022-03-28 15:26:56 +0200

    included further argument specifications in the grid

--- R/create_grid.R
+++ R/create_grid.R
@@ -28,18 +28,38 @@ create_grid <- function(x, at) {
     stop("'at' must be a list.")
   }
 
-  ### generate grid of all combinations of starting values and data sets
-  # if data sets exist, create a grid of starting values and data sets
-  if (is.list(x$data)) {
-    out <- expand.grid(at, 1:length(x$data))
-    colnames(out) <- c("at", "data_idx")
-  }
-  # if no data sets exist, simply return the starting values
-  else {
+  ### generate grid of all combinations of starting values, data sets, and further argument specifications
+  ## four cases:
+  ## 1. no data set and no further argument specifications
+  ## 2. one (or multiple) data sets and no further argument specifications
+  ## 3. no data set and further argument specifications
+  ## 4. one (or multiple) data sets and further argument specifications
+
+  # 1.
+  if (!is.list(x$data) & (any(lapply(x$f$add, length) <= 1))) {
     out <- data.frame(at = rep(NA, length(at)))
     out$at <- at
   }
+  # 2.
+  if (is.list(x$data) & (any(lapply(x$f$add, length) <= 1))) {
+    out <- expand.grid(at, 1:length(x$data))
+    colnames(out) <- c("at", "data_idx")
+  }
+  # 3.
+  if (!is.list(x$data) & (any(lapply(x$f$add, length) > 1))) {
+    # name of argument for which multiple values are given
+    des_argument <- names(which(lapply(x$f$add, length) > 1))
+    out <- expand.grid(at, x$f$add[[des_argument]])
+    colnames(out) <- c("at", "argument_value")
+  }
+  # 4.
+  if (is.list(x$data) & (any(lapply(x$f$add, length) > 1))) {
+    # name of argument for which multiple values are given
+    des_argument <- names(which(lapply(x$f$add, length) > 1))
+    out <- expand.grid(at, 1:length(x$data), x$f$add[[des_argument]])
+    colnames(out) <- c("at", "data_idx", "argument_value")
+  }
 
-  ### return the grid as data frame
+  ### return grid as data frame
   return(out)
 }
