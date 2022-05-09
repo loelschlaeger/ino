8f1ccac9241bf96bea616a9434cbf40467f19e6c
marius-oetting <marius.oetting@uni-bielefeld.de>
2022-05-09 16:41:11 +0200

    reactivate nr_optima function

--- R/evaluations.R
+++ R/evaluations.R
@@ -106,7 +106,45 @@ plot.ino <- function(x, var = ".time", by = ".strategy", type = "boxplot", ...) 
     out_plot <- ggplot(optimisation_df, aes(x = .data[[var]])) +
       geom_bar(aes(colour = .data[[by]], fill = .data[[by]]), position = "dodge")
   }
-  print(out_plot)
+  suppressWarnings(print(out_plot))
   return(out_plot)
 }
 
+
+#' @description
+#' This function provides an overview of the number of optima found with respect to
+#' the initialization strategies.
+#'
+#' @param x
+#' An object of class \code{ino}.
+#' @param plot
+#' Set to \code{TRUE} for plotting a frequency plot of found optima.
+#'
+#' @return
+#' A ggplot.
+#'
+#' @export
+#'
+#' @examples
+#' # nr_optima()
+#'
+#' @importFrom rlang .data
+#' @importFrom ggplot2 ggplot aes geom_bar theme_minimal
+#'
+#' @keywords
+#' evaluation
+
+nr_optima <- function(x, plot = FALSE, round = 2) {
+  optima_found <- unlist(sapply(x$optimizations, '[[', "res")["minimum", ])
+  optima_found <- round(optima_found, digits = round)
+  out  <- as.data.frame(table(optima_found))
+  colnames(out) <- c("optimum", "frequency")
+
+  if(plot){
+    out_plot <- ggplot2::ggplot(out, ggplot2::aes(x = .data$optimum, y = .data$frequency)) +
+      ggplot2::geom_bar(stat = "identity") +
+      ggplot2::theme_minimal()
+    print(out_plot)
+  }
+  return(out)
+}
