3fbb87f7d0197abc6b5ddc6031925803ec813c1c
loelschlaeger <lennart.oelschlaeger@uni-bielefeld.de>
2022-06-29 14:34:53 +0200

    Added tests.

---
+++ tests/testthat/test-functions.R
@@ -0,0 +1,44 @@
+test_that("ackley function works", {
+  expect_equal(f_ackley(c(0, 0)), 0)
+})
+
+test_that("beale function works", {
+  expect_equal(f_beale(c(3, 0.5)), 0)
+})
+
+test_that("matyas function works", {
+  expect_equal(f_matyas(c(0, 0)), 0)
+})
+
+test_that("easom function works", {
+  expect_equal(f_easom(c(pi, pi)), -1)
+})
+
+test_that("HMM likelihood works", {
+  ll <- f_ll_hmm(theta = c(-1, -1, 1, 2), data = ino::earthquakes, N = 2)
+  expect_type(ll, "double")
+})
+
+test_that("MNP likelihood works", {
+  data <- ino::probit_data[[1]]
+  theta <- attr(data, "true")
+  ll <- f_ll_mnp(theta = theta, data = data)
+  expect_type(ll, "double")
+})
+
+test_that("MNP data simulation works", {
+  data <- sim_mnp(N = 3, T = 2, b = c(1,-1), Omega = diag(2), Sigma = diag(2))
+  expect_true(is.data.frame(data))
+})
+
+test_that("MNL likelihood works", {
+  data <- ino::logit_data[[1]]
+  theta <- attr(data, "true")
+  ll <- f_ll_mnl(theta = theta, data = data)
+  expect_type(ll, "double")
+})
+
+test_that("MNL data simulation works", {
+  data <- sim_mnl(N = 3, T = 2, J = 3, b = c(-2,0.5,2), Omega = diag(3))
+  expect_true(is.data.frame(data))
+})
--- tests/testthat/test-setup.R
+++ tests/testthat/test-setup.R
@@ -1,4 +1,4 @@
-test_that("setup ino works", {
+test_that("ino setups work", {
   out <- setup_ino(
     f = ino:::f_ll_hmm,
     npar = 4,
@@ -6,7 +6,32 @@ test_that("setup ino works", {
     N = 2,
     neg = TRUE,
     opt = set_optimizer_nlm(),
-    verbose = FALSE
+    verbose = FALSE,
+    skip_test = FALSE
   )
   expect_s3_class(out, "ino")
+  expect_type(grid_ino(out), "list")
+  expect_s3_class(clear_ino(out), "ino")
+  expect_s3_class(merge_ino(out,out), "ino")
+})
+
+test_that("set optimizer works", {
+  opt <- set_optimizer(
+    opt = pracma::nelder_mead,
+    f = "fn",
+    p = "x0",
+    v = "fmin",
+    z = "xmin",
+    tol = 1e-6,
+    crit = c("xmin", "fcount")
+  )
+  expect_s3_class(opt, "optimizer")
+})
+
+test_that("set optimizer nlm works", {
+  expect_s3_class(set_optimizer_nlm(), "optimizer")
+})
+
+test_that("set optimizer optim works", {
+  expect_s3_class(set_optimizer_optim(), "optimizer")
 })
