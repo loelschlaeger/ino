2523df1b8eb5eb1f529cea1fae938f33f0c2ca99
lennart.oelschlaeger <oelschlaeger.lennart@gmail.com>
2022-09-27 14:41:44 +0200

    Removed jss template.

--- jss/ino_oelschlaeger_oetting_bauer.R
+++
@@ -1,9 +0,0 @@
-### R code from vignette source 'ino_oelschlaeger_oetting_bauer.Rnw'
-
-###################################################
-### code chunk number 1: preliminaries
-###################################################
-options(prompt = "> ", continue = "+  ", width = 70, useFancyQuotes = FALSE)
-library("ino")
-
-
--- jss/workflow.R
+++
@@ -1,4 +0,0 @@
-try(setwd("jss"),silent=TRUE)
-Sweave("ino_oelschlaeger_oetting_bauer.Rnw", encoding = "utf8")
-tinytex::pdflatex("ino_oelschlaeger_oetting_bauer.tex")
-Stangle("ino_oelschlaeger_oetting_bauer.Rnw", encoding = "utf8")
