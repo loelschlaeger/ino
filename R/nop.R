ce214edd5b6cd9e64ae76078ee4a1816cc83042e
lennart.oelschlaeger <oelschlaeger.lennart@gmail.com>
2023-01-18 15:13:33 +0100

    relocated S3 methods

--- R/nop.R
+++ R/nop.R
@@ -1344,3 +1344,24 @@ Nop <- R6::R6Class(
 
   )
 )
+
+#' @noRd
+#' @exportS3Method
+
+print.Nop <- function(x, ...) {
+  x$print(...)
+}
+
+#' @noRd
+#' @exportS3Method
+
+summary.Nop <- function(object, ...) {
+  object$summary(...)
+}
+
+#' @noRd
+#' @exportS3Method
+
+plot.Nop <- function(x, ...) {
+  x$plot(...)
+}
