5cc8df60e68341841242d8943d41bbddc9623492
Lennart Oelschläger <oelschlaeger.lennart@gmail.com>
2023-05-29 20:30:36 +0200

    Add probit vignette (#25)

--- R/data.R
+++ R/data.R
@@ -38,19 +38,19 @@
 #' dataset
 "hmm_ino"
 
-# #' Example application of \{ino\} to probit likelihood
-# #'
-# #' @description
-# #' See the vignette about the probit likelihood for details:
-# #' \code{vignette("example_probit", package = "ino")}
-# #'
-# #' @docType data
-# #'
-# #' @usage data(probit_ino)
-# #'
-# #' @format
-# #' A \code{\link{Nop}} object.
-# #'
-# #' @keywords
-# #' dataset
-# "probit_ino"
+#' Example application of to probit likelihood
+#'
+#' @description
+#' See the vignette about the probit likelihood for details:
+#' \code{vignette("example_probit", package = "ino")}
+#'
+#' @docType data
+#'
+#' @usage data(probit_ino)
+#'
+#' @format
+#' A \code{\link{Nop}} object.
+#'
+#' @keywords
+#' dataset
+"probit_ino"
