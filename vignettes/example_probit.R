281e82887bcd11069a59dd5957c8ae63d7ed1f3f
Oelschläger <lennart.oelschlaeger@uni-bielefeld.de>
2023-05-29 19:55:54 +0200

    probit model simulation results

--- vignettes/example_probit.R
+++ vignettes/example_probit.R
@@ -1,16 +1,14 @@
 ## ---- setup, include = FALSE--------------------------------------------------------
-#> knitr::opts_chunk$set(
-#>   collapse = TRUE,
-#>   comment = "#>",
-#>   fig.align = "center",
-#>   fig.dim = c(8, 6),
-#>   out.width = "75%",
-#>   eval = FALSE
-#> )
-#> # library("ino")
-devtools::load_all() # remove later
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  fig.align = "center",
+  fig.dim = c(8, 6),
+  out.width = "75%",
+  eval = FALSE
+)
+library("ino")
 options("ino_verbose" = TRUE)
-options("ino_verbose" = TRUE, "ino_ncores" = 5) # REMOVE later
 
 ## ---- choice covariates, eval = TRUE------------------------------------------------
 X <- function(n, t) {
@@ -21,8 +19,8 @@ X(n = 1, t = 1)
 
 
 ## ---- simulate data, eval = TRUE----------------------------------------------------
-N <- 200
-T <- 30
+N <- 100
+T <- 20
 b <- c(1, -10)
 Omega <- matrix(c(0.2, 0.5, 0.5, 2), 2, 2)
 Sigma <- matrix(c(1, -0.5, 0.2, -0.5, 1, 0.2, 0.2, 0.2, 1), 3, 3)
@@ -55,13 +53,13 @@ print(probit_ino)
 
 
 ## ---- optimize with random initial values-------------------------------------------
-probit_ino$optimize(initial = "random", runs = 40, label = "random")
+probit_ino$optimize(initial = "random", runs = 100, label = "random")
 
 
 ## -----------------------------------------------------------------------------------
 probit_ino$
   reduce("data", how = "random", proportion = 0.2)$
-  optimize(initial = "random", runs = 40, label = "subset")$
+  optimize(initial = "random", runs = 100, label = "subset")$
   reset_argument("data")$
   continue()
 
@@ -69,6 +67,16 @@ probit_ino$
 ## -----------------------------------------------------------------------------------
 probit_ino$
   standardize("data", by_column = TRUE, ignore = 1:3)$
-  optimize(initial = "random", runs = 40, label = "standardized")$
+  optimize(initial = "random", runs = 100, label = "standardized")$
   reset_argument("data")
+
+
+## -----------------------------------------------------------------------------------
+probit_ino$
+  standardize("data", by_column = TRUE, ignore = 1:3)$
+  reduce("data", how = "random", proportion = 0.2)$
+  optimize(initial = "random", runs = 100, label = "standardized_subset")$
+  reset_argument("data")$
+  standardize("data", by_column = TRUE, ignore = 1:3)$
+  continue()
 
