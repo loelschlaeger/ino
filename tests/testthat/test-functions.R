7ce8163e58395b964845a9afcb4085bb4ae8d7d2
Lennart Oelschläger <oelschlaeger.lennart@gmail.com>
2023-05-31 08:37:28 +0000

    Styled package :pencil:

--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -15,9 +15,9 @@ test_that("easom function can be evaluated", {
 })
 
 test_that("HMM data simulation and likelihood computation works", {
-  tpm <- matrix(c(0.8,0.1,0.2,0.9), nrow = 2)
-  mu <- c(-2,2)
-  sigma <- c(0.5,1)
+  tpm <- matrix(c(0.8, 0.1, 0.2, 0.9), nrow = 2)
+  mu <- c(-2, 2)
+  sigma <- c(0.5, 1)
   theta <- c(log(tpm[row(tpm) != col(tpm)]), mu, log(sigma))
   data <- sim_hmm(Tp = 100, N = 2, theta = theta)
   ll <- f_ll_hmm(theta = theta, data = data, N = 2)
