e50e2ebde673294e88021694d2f189d4b3995ecf
lennart.oelschlaeger <oelschlaeger.lennart@gmail.com>
2023-11-09 13:33:30 +0100

    added startup message

--- R/ino-package.R
+++ R/ino-package.R
@@ -4,3 +4,16 @@
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
+
+#' @noRd
+#' @importFrom cli style_hyperlink
+#' @importFrom utils packageVersion
+
+.onAttach <- function(lib, pkg) {
+  msg <- c(
+    paste0("Thanks for using {ino} version ", utils::packageVersion("ino")), "!\n",
+    "See ", cli::style_hyperlink("https://loelschlaeger.de/ino", "https://loelschlaeger.de/ino") ," for help."
+  )
+  packageStartupMessage(msg)
+  invisible()
+}
